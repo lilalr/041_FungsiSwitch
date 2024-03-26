fb43b1e290b69104c13aa63848e95ec9d40c1ee6
lilalr <gurennut@gmail.com>
2024-03-26 17:37:19 +0700

    membuat prosedur input

--- 041_FungsiSwitch/041_FungsiSwitch.cpp
+++ 041_FungsiSwitch/041_FungsiSwitch.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 int bilA, bilB;
 
+void input()
+{
+    cout << "Masukkan Bilangan Pertama = ";
+    cin >> bilA;
+    cout << "Masukkan Bilangan Kedua = ";
+    cin >> bilB;
+}
+
