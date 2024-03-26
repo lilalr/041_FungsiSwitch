dbf66fc00bd85bec58412f2287cd9441ef846438
lilalr <gurennut@gmail.com>
2024-03-26 17:48:01 +0700

    memasukkan variabel pilihan

--- 041_FungsiSwitch/041_FungsiSwitch.cpp
+++ 041_FungsiSwitch/041_FungsiSwitch.cpp
@@ -31,3 +31,20 @@ float pembagian(float a, float b)
     return a / b;
 }
 
+int main()
+{
+    int pilihan;
+    do
+    {
+        cout << "Kalkulator Sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukan menu pilihan : ";
+        cin >> pilihan;
+
+        
